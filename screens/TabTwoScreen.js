076d24ee390f40a0b43d809af68934b7b70e1b75
Wesley Scholl <clients@konjoweb.com>
2023-01-19 23:22:47 -0500

    added color gradient

--- screens/TabTwoScreen.js
+++ screens/TabTwoScreen.js
@@ -5,6 +5,7 @@ import { useTheme } from '@react-navigation/native';
 import MIcon from "@expo/vector-icons/MaterialIcons";
 import * as Animatable from 'react-native-animatable';
 import * as Haptics from 'expo-haptics';
+import { LinearGradient } from 'expo-linear-gradient'
 
 AnimatableView = Animatable.createAnimatableComponent(View);
 
@@ -12,12 +13,12 @@ const size = 275 ;
 const symbolSize = 16;
 const radius = size / 2;
 const center = radius;
-let clockCall;
 
-export function Tick({x, y, deg}){
+export function Tick({x, y, deg, color}){
   return (
-    <View style={{borderRadius: 5, left: x, top: y, position:'absolute',
-        height: 8, width: 70, backgroundColor: "#fff", transform: [{rotate: `${deg}deg`}]
+    <LinearGradient 
+        colors={['#f7886b', '#db3f73']} style={{borderRadius: 5, left: x, top: y, position:'absolute',
+        height: 8, width: 70, transform: [{rotate: `${deg}deg`}]
         }} />
       )
   }
@@ -25,23 +26,16 @@ export function Tick({x, y, deg}){
 export default function App({navigation}) {
   let colorScheme = useColorScheme();
   const { colors } = useTheme();
-  const [timer, setTimer] = useState(30)
+  const [counter, setCounter] = useState(30)
    const [degrees, setDegrees] = useState([0, 9, 18, 27, 36, 45, 54, 63, 72, 81, 90, 99, 108, 117, 126, 135, 144, 153, 162, 
     171, 180, 189, 198, 207, 216, 225, 234, 243, 252, 261, 270, 279, 288, 297, 306, 315, 324, 333, 342, 351, 360])
     const colorList = [{offset: '40%', color: '#525461', opacity: '0.3'},{offset: '75%', color: '#3c3f5f', opacity: '0.6'}]
 
     useEffect(() => {
-     clockCall = setInterval(() => {
-        decrementClock();
-        clearInterval(clockCall);
-      }, 1000);
-      
-    }, []);
-
-   const decrementClock = () => {      
-    setTimer(timer-1);
-   };
-
+      const timer =
+        counter > 0 && setInterval(() => {setCounter(counter - 1), Haptics.selectionAsync()}, 1000);
+      return () => clearInterval(timer);
+    }, [counter]);
 
 function getXY(deg){
   let angleRad = deg * Math.PI / 180;
@@ -59,13 +53,13 @@ function getXY(deg){
       }, 500)
     } 
   }
-console.log(timer)
+
     return (
       <View style={{ flex: 1, justifyContent:'center', alignItems:'center', backgroundColor: "#2e3048" }}>
-        <Text style={{position: "absolute", top: 100, color: colors.text, fontSize: 40}}>Countdown</Text>
+        <Text style={{position: "absolute", top: 60, color: '#fff', fontSize: 40}}>Countdown</Text>
         {/* <RadialGradient x="50%" y="50%" rx="50%" ry="50%" colorList={colorList} style={{position: 'absolute', zIndex: 2}}/> */}
         <View style={{ width: size, height: size, borderRadius: size / 2, position: "absolute"}}>
-        <Text style={{fontSize: 28, color: '#fff'}}>{timer === 0 ? 'Times Up!' : `${timer}`}</Text>
+        <Text style={{fontSize: 28, color: '#fff', alignSelf: "center", position: "absolute", top: 100}}>{counter === 0 ? 'Times Up!' : `${counter}`}</Text>
         {degrees && degrees.length > 0 && (degrees.map((deg, id) => { 
           let xandy = getXY(deg)
          return <AnimatableView
@@ -74,8 +68,7 @@ console.log(timer)
            delay={id * 500}
            duration={500}
            useNativeDriver={true}
-           onAnimationEnd={() => endAnim(id)}
-           onAnimationBegin={() => Haptics.selectionAsync()}>
+           onAnimationEnd={() => endAnim(id)}>
             <Tick x={xandy.x} y={xandy.y} deg={deg} />
            </AnimatableView>
         }))}
